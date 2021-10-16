d9d010da913a870c5d35406956aa9c7f5f4143b1
Dungnv0106 <dungzboo@gmail.com>
2021-10-16 14:37:11 +0700

    first commit

---
+++ Array.js
@@ -0,0 +1,17 @@
+var hocSinh1 = {
+    name: 'Hoang', 
+    age: 19,
+    gender: 'Male'
+};
+var hocSinh2 = {
+    name: 'Nam',
+    age: 20,
+    gender: 'Male'
+};
+var hocSinh3 = {
+    name: 'Thuong',
+    age: 20,
+    gender: 'Female',
+};
+var hocSinh = [hocSinh1, hocSinh2, hocSinh3];
+console.log(hocSinh);
